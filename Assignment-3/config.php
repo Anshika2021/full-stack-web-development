d4cccd0a91b4721b555450ff36fb1fd3a6989dee
Anshika2021 <77603417+Anshika2021@users.noreply.github.com>
2021-02-15 12:46:12 +0530

    Add files via upload

---
+++ Assignment-3/config.php
@@ -0,0 +1,17 @@
+<?php
+$hostname="127.0.0.1";
+$username="root";
+$password="";
+$databasename="web";
+
+$conn= mysqli_connect($hostname, $username, $password, $databasename);
+if($conn){
+    echo"Connection Successful...";
+}
+else{
+    echo"Connection failed..." +mysqli_connect_error();
+}
+?>
+
+
+
